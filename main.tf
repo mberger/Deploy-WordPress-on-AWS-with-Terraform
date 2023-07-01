bfe4359190a92b844e1b9112d598c18ea57d333f
kovacsmate88 <kovacsmate321@gmail.com>
2023-07-01 16:50:14 +0200

    refactor

--- main.tf
+++ main.tf
@@ -1,146 +1,22 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
-terraform {
-  backend "s3" {
-    bucket = "terraforming-mars"
-    key = "terraform.tfstate"
-    region = "eu-west-1"
-    dynamodb_table = "terraforming-mars"
-  }
-}
-
-resource "aws_vpc" "mars_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "mars-vpc"
-  }
-}
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.mars_vpc.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "eu-west-1a"
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.mars_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-west-1b"
-}
-
-resource "aws_security_group" "instance_sg" {
-  name        = "wordpress-sg"
-  description = "Security group for WordPress EC2 instance"
-
-  vpc_id = aws_vpc.mars_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_internet_gateway" "mars_igw" {
-  vpc_id = aws_vpc.mars_vpc.id
-}
-
-resource "aws_route_table" "mars_route_table" {
-  vpc_id = aws_vpc.mars_vpc.id
-}
-
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.mars_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.mars_igw.id
-}
-
-resource "aws_route_table_association" "rtb_association" {
-  route_table_id = aws_route_table.mars_route_table.id
-  subnet_id      = aws_subnet.public_subnet.id
-}
-
-resource "aws_s3_bucket" "wp_bucket" {
-  bucket = "wp-mars-bucket"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_ownership_controls" "bucket_owner_controll" {
-  bucket = aws_s3_bucket.wp_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.bucket_owner_controll]
-
-  bucket = aws_s3_bucket.wp_bucket.id
-  acl    = "private"
-}
-
-# Upload files and directories from a local directory to an S3 bucket
-resource "null_resource" "upload_media_files" {
-  depends_on = [aws_s3_bucket.wp_bucket]
-
-  provisioner "local-exec" {
-    command = "aws s3 sync '/home/mate/Dokumentumok' 's3://${aws_s3_bucket.wp_bucket.bucket}/'"
-  }
-}
-
-
-resource "aws_db_instance" "wordpress_db" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "8.0.32"
-  instance_class       = "db.t2.micro"
-  db_name              = "wordpress_db"
-  username             = "a"
-  password             = "adminadmin"
-  port                 = 3306
-  multi_az             = false
-  skip_final_snapshot  = true
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-  db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
-}
-
-resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "wordpress-db-subnet-group"
-  subnet_ids = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
-}
-
-resource "aws_instance" "wordpress_instance" {
-  ami                   = "ami-01dd271720c1ba44f"
-  instance_type         = "t2.micro"
-  key_name              = "MYKEYEC2"
-  subnet_id             = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+module aws_wordpress {
+    source              = "./latest"
+    database_name           = var.database_name   // database name
+    database_user           = var.database_user // database username
+    // Password here will be used to create master db user.It should be changed later
+    database_password = var.database_password // password for user database
+    shared_credentials_file = var.shared_credentials_file         // Access key and Secret key file location
+    region                  = var.region // Ireland region
+    // avaibility zone and their CIDR
+    AZ1          = var.AZ1 // for EC2
+    AZ2          = var.AZ2 // for RDS 
+    AZ3          = var.AZ3 // for RDS
+    VPC_cidr     = var.VPC_cidr     // VPC CIDR
+    subnet1_cidr = var.subnet1_cidr     // Public Subnet for EC2
+    subnet2_cidr = var.subnet2_cidr     //Private Subnet for RDS
+    subnet3_cidr = var.subnet3_cidr     //Private subnet for RDS
+    instance_type    = var.instance_type    //type of instance
+    instance_class   = var.instance_class //type of RDS Instance
+    PUBLIC_KEY_PATH  = var.PUBLIC_KEY_PATH // key name for ec2, make sure it is created before terraform apply
+    PRIV_KEY_PATH    = var.PRIV_KEY_PATH
+    root_volume_size = var.root_volume_size
 }
