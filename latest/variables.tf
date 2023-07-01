1fe09a95d80b8f8b19b40820e207cce2e1b266b8
kovacsmate88 <kovacsmate321@gmail.com>
2023-07-01 16:50:02 +0200

    add values to the variables in the variables.tf file

--- latest/variables.tf
+++ latest/variables.tf
@@ -1,18 +1,86 @@
-variable "database_name" {}
-variable "database_password" {}
-variable "database_user" {}
+variable "database_name" {
+    description = "Name of the database"
+    type = string
+    default = "admin"
+}
+variable "database_password" {
+    description = "Password for the database user"
+    type = string
+    default = "password"
+}
+variable "database_user" {
+    description = "Username for the database"
+    type        = string
+    default     = "admin"
+}
 
-variable "region" {}
-variable "shared_credentials_file" {}
-variable "AZ1" {}
-variable "AZ2" {}
-variable "AZ3" {}
-variable "VPC_cidr" {}
-variable "subnet1_cidr" {}
-variable "subnet2_cidr" {}
-variable "subnet3_cidr" {}
-variable "instance_type" {}
-variable "instance_class" {}
-variable "PUBLIC_KEY_PATH" {}
-variable "PRIV_KEY_PATH" {}
-variable "root_volume_size" {}
+variable "region" {
+    description = "AWS region"
+    type        = string
+    default     = "eu-west-1"
+}
+variable "shared_credentials_file" {
+    description = "Location of the AWS credentials file"
+    type        = string
+    default     = "~/.aws/credentials"
+}
+variable "AZ1" {
+    description = "Availability Zone 1"
+    type        = string
+    default     = "eu-west-1a"
+}
+variable "AZ2" {
+    description = "Availability Zone 2"
+    type        = string
+    default     = "eu-west-1b"
+}
+variable "AZ3" {
+    description = "Availability Zone 3"
+    type        = string
+    default     = "eu-west-1c"
+}
+variable "VPC_cidr" {
+    description = "CIDR block for the VPC"
+    type        = string
+    default     = "10.0.0.0/16"
+}
+variable "subnet1_cidr" {
+    description = "CIDR block for Subnet 1"
+    type        = string
+    default     = "10.0.0.0/24"
+}
+variable "subnet2_cidr" {
+    description = "CIDR block for Subnet 2"
+    type        = string
+    default     = "10.0.1.0/24"
+}
+variable "subnet3_cidr" {
+    description = "CIDR block for Subnet 3"
+    type        = string
+    default     = "10.0.2.0/24"
+}
+variable "instance_type" {
+    description = "Type of EC2 instance"
+    type        = string
+    default     = "t2.micro"
+}
+variable "instance_class" {
+    description = "Type of RDS instance"
+    type        = string
+    default     = "db.t2.micro"
+}
+variable "PUBLIC_KEY_PATH" {
+    description = "Path to the public key file"
+    type        = string
+    default     = "~/.ssh/MYKEYEC2.pub"
+}
+variable "PRIV_KEY_PATH" {
+    description = "Path to the private key file"
+    type        = string
+    default     = "~/.ssh/MYKEYEC2.pem"
+}
+variable "root_volume_size" {
+    description = "Size of the root volume"
+    type        = number
+    default     = 22
+}
