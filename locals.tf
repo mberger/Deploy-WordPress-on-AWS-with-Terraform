ecb9b7ed2f8564a3b3b5b51e6694d238814a82c7
kovacsmate88 <kovacsmate321@gmail.com>
2023-07-13 18:17:36 +0200

    add locals.tf

---
+++ locals.tf
@@ -0,0 +1,7 @@
+locals {
+  database_name         = file("${path.module}/secrets/database_name.txt")
+  database_password     = file("${path.module}/secrets/database_password.txt")
+  database_user         = file("${path.module}/secrets/database_user.txt")
+  aws_access_key_id     = file("${path.module}/secrets/aws_access_key_id.txt")
+  aws_secret_access_key = file("${path.module}/secrets/aws_secret_access_key.txt")
+}
