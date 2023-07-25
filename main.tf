f4b3a69ebf1b064b94636edafc248c34c5c5ee5e
kovacsmate88 <kovacsmate321@gmail.com>
2023-07-25 23:05:38 +0200

    refactor and add hints

--- main.tf
+++ main.tf
@@ -252,10 +252,25 @@ resource "aws_s3_bucket_ownership_controls" "bucket_owner_controll" {
   }
 }
 
+// with this part u can change the access of the s3 bucket
+/* resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+  bucket = aws_s3_bucket.wp_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+ */
+
 resource "aws_s3_bucket_acl" "bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.bucket_owner_controll]
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket_owner_controll
+    //aws_s3_bucket_public_access_block.bucket_public_access
+    ]
 
   bucket = aws_s3_bucket.wp_bucket.id
+  // if u want to change the bucket to public-read do this: acl = "public-read"
   acl    = "private"
 }
 
