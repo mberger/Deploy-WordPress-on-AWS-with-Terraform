601ac7ec30cc55a095854efa8332f7a7aa660b7c
kovacsmate88 <kovacsmate321@gmail.com>
2023-09-02 20:05:33 +0200

    add new local

--- locals.tf
+++ locals.tf
@@ -4,4 +4,6 @@ locals {
   database_user         = file("${path.module}/secrets/database_user.txt")
   aws_access_key_id     = file("${path.module}/secrets/aws_access_key_id.txt")
   aws_secret_access_key = file("${path.module}/secrets/aws_secret_access_key.txt")
+  is_windows = "if [ $(uname) == 'Darwin' ]; then echo false; elif [ $(expr substr $(uname -s) 1 5) == 'Linux' ]; then echo false; elif [ $(expr substr $(uname -s) 1 10) == 'MINGW32_NT' ]; then echo true; elif [ $(expr substr $(uname -s) 1 10) == 'MINGW64_NT' ]; then echo true; else echo false; fi"
+  
 }
