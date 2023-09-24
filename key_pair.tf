8f984c462973deb4723788efb807523f09bde9e0
kovacsmate88 <kovacsmate321@gmail.com>
2023-09-25 00:52:22 +0200

    generate key pairs

---
+++ key_pair.tf
@@ -0,0 +1,10 @@
+// Create private key to SSH into EC2
+resource "tls_private_key" "private_key" {
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name = var.key_name
+  public_key = tls_private_key.private_key.public_key_openssh
+}
