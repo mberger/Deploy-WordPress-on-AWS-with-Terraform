84fc573b4c03548a23f15b45e4cad5dfff5ec7fc
kovacsmate88 <kovacsmate321@gmail.com>
2023-09-24 23:31:33 +0200

    rename the file

---
+++ wordpress_db.tf
@@ -0,0 +1,28 @@
+  // Create RDS Subnet group
+  resource "aws_db_subnet_group" "db_subnet_group" {
+    name       = "wordpress-db-subnet-group"
+    subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  }
+
+  // Create RDS instance
+  resource "aws_db_instance" "wordpress_db" {
+    allocated_storage    = 10
+    engine               = "mysql"
+    engine_version       = "8.0.32"
+    instance_class       = var.instance_class
+    db_name              = local.database_name
+    username             = local.database_user
+    password             = local.database_password
+    skip_final_snapshot  = true
+    vpc_security_group_ids = [aws_security_group.RDS_allow_rule.id]
+    db_subnet_group_name = aws_db_subnet_group.db_subnet_group.id
+
+    // make sure the rds manual password changes is ignored
+    lifecycle {
+      ignore_changes = [ password ]
+    }
+
+    tags = {
+      Name = "wordpress-database"
+    }
+  }
