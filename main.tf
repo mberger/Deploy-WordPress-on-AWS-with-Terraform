516530cf674068b6e8ae89aabdd7938e8b63474b
kovacsmate88 <kovacsmate321@gmail.com>
2023-10-01 13:05:55 +0200

    reformat

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 // Configure the AWS provider
-  provider "aws" {
-    region = var.region
-    profile = "default"
-  }
+provider "aws" {
+  region  = var.region
+  profile = "default"
+}
 
-  // Random provider to create random suffix for the S3 wordpress bucket
-  provider "random" {
-    // (No specific configurations needed for the Random provider)
-  }
+// Random provider to create random suffix for the S3 wordpress bucket
+provider "random" {
+  // (No specific configurations needed for the Random provider)
+}
 
-  // Configure Terraform backend to store state files on S3 and lock with DynamoDB
-  terraform {
-    backend "s3" {
-      bucket = "wordpress-state-files"    // name of your S3 bucket
-      key = "terraform.tfstate"       // Dont need to touch it, it's good ;)
-      region = "eu-west-1"            // name of your region
-      dynamodb_table = "wordpress-state-locking-service"   // name of your dynamo database table
-    }
+// Configure Terraform backend to store state files on S3 and lock with DynamoDB
+terraform {
+  backend "s3" {
+    bucket         = "wordpress-state-files"           // name of your S3 bucket
+    key            = "terraform.tfstate"               // Dont need to touch it, it's good ;)
+    region         = "eu-west-1"                       // name of your region
+    dynamodb_table = "wordpress-state-locking-service" // name of your dynamo database table
   }
+}
